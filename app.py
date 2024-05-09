1f059de0861ac92651b241032e1eba056814d801
isao <86751458+isaotoyama@users.noreply.github.com>
2024-05-09 10:01:42 -0700

    update the
    with open(json_file_path, 'w', encoding='utf-8') as jsonf:
    with open(json_file_path, 'w', encoding='utf-8-sig') as jsonf:

--- app.py
+++ app.py
@@ -9,11 +9,11 @@ json_file_path = 'data/cs.json'
 
 # Reading the CSV and adding the data to a dictionary
 data = []
-with open(csv_file_path, encoding='utf-8') as csvf:
+with open(csv_file_path, encoding='utf-8-sig') as csvf:
     csv_reader = csv.DictReader(csvf)
     for row in csv_reader:
         data.append(row)
 
 # Writing the dictionary to a JSON file
-with open(json_file_path, 'w', encoding='utf-8') as jsonf:
+with open(json_file_path, 'w', encoding='utf-8-sig') as jsonf:
     jsonf.write(json.dumps(data, indent=4))
